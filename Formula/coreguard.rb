3889ee2ca8476bbc8767b6338658c5c502132a01
Chris Buckminster <chrs.buckmnster@gmail.com>
2026-02-26 16:20:39 -0500

    Add Homebrew formula for tap distribution
    Enables installation via:
      brew tap chris-buckminster/coreguard && brew install coreguard

---
+++ Formula/coreguard.rb
@@ -0,0 +1,121 @@
+class Coreguard < Formula
+  include Language::Python::Virtualenv
+
+  desc "DNS-based ad/tracker blocking for macOS"
+  homepage "https://github.com/chris-buckminster/coreguard"
+  url "https://github.com/chris-buckminster/coreguard/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "ae6ef83e257f0398123a224acb4963d7a577a5f709c1cdc1fe933b65ae476d7b"
+  license "MIT"
+
+  depends_on :macos
+  depends_on "python@3.12"
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/96/f0/5eb65b2bb0d09ac6776f2eb54adee6abe8228ea05b20a5ad0e4945de8aac/anyio-4.12.1.tar.gz"
+    sha256 "41cfcc3a4c85d3f05c932da7c26d0201ac36f72abd4435ba90d0464a3ffed703"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/af/2d/7bf41579a8986e348fa033a31cdd0e4121114f6bce2457e8876010b092dd/certifi-2026.2.25.tar.gz"
+    sha256 "e887ab5cee78ea814d3472169153c2d12cd43b14bd03329a39a9c6e2e80bfba7"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/3d/fa/656b739db8587d7b5dfa22e22ed02566950fbfbcdc20311993483657a5c0/click-8.3.1.tar.gz"
+    sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
+  end
+
+  resource "dnslib" do
+    url "https://files.pythonhosted.org/packages/a2/71/269f74ef9bc8ca453af2e1768d4f4c8e7ef5f894d058d27fd1b69c754d7f/dnslib-0.9.26.tar.gz"
+    sha256 "be56857534390b2fbd02935270019bacc5e6b411d156cb3921ac55a7fb51f1a8"
+  end
+
+  resource "h2" do
+    url "https://files.pythonhosted.org/packages/1d/17/afa56379f94ad0fe8defd37d6eb3f89a25404ffc71d4d848893d270325fc/h2-4.3.0.tar.gz"
+    sha256 "6c59efe4323fa18b47a632221a1888bd7fde6249819beda254aeca909f221bf1"
+  end
+
+  resource "hpack" do
+    url "https://files.pythonhosted.org/packages/2c/48/71de9ed269fdae9c8057e5a4c0aa7402e8bb16f2c6e90b3aa53327b113f8/hpack-4.1.0.tar.gz"
+    sha256 "ec5eca154f7056aa06f196a557655c5b009b382873ac8d1e66e79e87535f1dca"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+    sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+  end
+
+  resource "hyperframe" do
+    url "https://files.pythonhosted.org/packages/02/e7/94f8232d4a74cc99514c13a9f995811485a6903d48e5d952771ef6322e30/hyperframe-6.1.0.tar.gz"
+    sha256 "f630908a00854a7adeabd6382b43923a4c4cd4b821fcb527e6ab9e15382a3b08"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
+  end
+
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/82/30/31573e9457673ab10aa432461bee537ce6cef177667deca369efb79df071/tomli-2.4.0.tar.gz"
+    sha256 "aa89c3f6c277dd275d8e243ad24f3b5e701491a860d5121f2cdd399fbb31fc9c"
+  end
+
+  resource "tomli-w" do
+    url "https://files.pythonhosted.org/packages/19/75/241269d1da26b624c0d5e110e8149093c759b7a286138f4efd61a60e75fe/tomli_w-1.2.0.tar.gz"
+    sha256 "2dd14fac5a47c27be9cd4c976af5a12d87fb1f0b4512f81d69cce3b35ae25021"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  def post_install
+    (etc/"coreguard").mkpath
+    (etc/"coreguard/blocklists").mkpath
+
+    touch etc/"coreguard/custom-allow.txt" unless (etc/"coreguard/custom-allow.txt").exist?
+    touch etc/"coreguard/custom-block.txt" unless (etc/"coreguard/custom-block.txt").exist?
+  end
+
+  def caveats
+    <<~EOS
+      To start coreguard as a DNS blocker, you need root privileges:
+
+        sudo coreguard install
+
+      This installs a launchd service that starts coreguard on boot.
+
+      Alternatively, run manually:
+
+        sudo coreguard start
+
+      To manage the Homebrew service (runs as root):
+
+        sudo brew services start coreguard
+
+      Config directory: #{etc}/coreguard/
+    EOS
+  end
+
+  service do
+    run [opt_bin/"coreguard", "start", "--foreground"]
+    keep_alive true
+    require_root true
+    log_path var/"log/coreguard.log"
+    error_log_path var/"log/coreguard.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/coreguard --version")
+  end
+end
